f2ca4166e9de25fc3f1d35ee5ce1695125919cb4
Hossein Nikpour <nikpour.hossein@yahoo.com>
2017-07-03 15:40:03 +0430

    Added DateTime type

---
+++ src/components/DateTime.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block">
+     <label>{{field.Title}}</label>:
+    <el-date-picker
+      v-model="value"
+      type="date"
+      placeholder="Pick a day">
+    </el-date-picker>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+    props: ['field'],
+    data () {
+        return {
+            value: ''
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
+
--- src/components/Field.js
+++ src/components/Field.js
@@ -2,13 +2,14 @@ import TextField from './Text'
 import NumberField from './Number'
 import BooleanField from './Boolean'
 import SelectField from './Select'
-// import CustomSelectField from './CustomSelect'
+import CustomSelectField from './CustomSelect'
+import DateTimeField from './DateTime'
 
 export default {
-    components: { TextField, NumberField, BooleanField, SelectField },
+    components: { TextField, NumberField, BooleanField, SelectField, CustomSelectField, DateTimeField },
     props: ['field', 'data'],
     render () {
-        switch (this.field.TypeAsString) {
+        switch (this.field.Type) {
         case 'Text':
             return <TextField ref="field" field={this.field}></TextField>
         case 'Number':
@@ -16,19 +17,28 @@ export default {
         case 'Boolean':
             return <BooleanField ref="field" field={this.field}></BooleanField>
         case 'Lookup':
-            return <SelectField ref="field" field={this.field} options={this.options}></SelectField>
-        // case 'CustomLookup':
-        //     return <CustomSelectField ref="field" field={this.field} options={this.options} data={this.data}></CustomSelectField>
+            return <SelectField ref="field" field={this.field} onChange={this.change}></SelectField>
+        case 'DateTime':
+            return <DateTimeField ref="field" field={this.field}></DateTimeField>
+      //  case 'MD':
+       //     return <SelectField ref="field" field={this.field}></SelectField>
+        case 'RelatedCustomLookupQuery':
+            return <CustomSelectField ref="field" field={this.field} data={this.data}></CustomSelectField>
         default:
-           throw new Error(`Unexpected Type: ${this.field.TypeAsString}`)
+            // throw new Error(`Unexpected Type: ${this.field.Type}`)
+            console.log(`Unexpected Type: ${this.field.Type}`)
         }
     },
     computed: {
         value () {
-            return this.field.TypeAsString === 'Lookup'
+            return this.field.Type === 'Lookup'
             ? {[`${this.field.InternalName}Id`]: this.$refs.field.value}
             : {[this.field.InternalName]: this.$refs.field.value}
-            // return {[this.field.InternalName]: this.$refs.field.value}
+        }
+    },
+    methods: {
+        change (val) {
+            this.$emit('change', this.value)
         }
     }
 }
