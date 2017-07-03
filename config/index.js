6511a8783a0b90ae33fa5909fa7e29c037978b81
Hossein Nikpour <nikpour.hossein@yahoo.com>
2017-07-03 15:36:25 +0430

    Updated api addresses

--- config/index.js
+++ config/index.js
@@ -29,11 +29,14 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/_api': {
-        target: 'http://localhost:2601',
+        target: 'http://31.47.33.146:50',
         // changeOrigin: true,
         // pathRewrite: {
         //   '^/api': ''
         // }
+      },
+      '/_Layouts/15/BaseSolution/Services.aspx': {
+        target: 'http://31.47.33.146:50',
       }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
