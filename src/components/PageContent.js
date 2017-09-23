595a1e9cf564ecc7ebf68717f975fafc52c42136
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-09-23 14:49:11 +0000

    Modifying PageContent CSS structure

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -5,9 +5,11 @@ import { mapActions } from 'vuex'
 export default {
     template: `
         <el-row type='flex' justify='center' v-loading='loading'>
-            <el-col :span='18'>
+            <el-col :span='20'>
                 <PageTemplate/>
-                <el-button type='primary' @click='click'>زخیره</el-button>
+                <el-row type='flex' justify='right'>
+                    <el-button type='primary' @click='click'>ذخیره</el-button>
+                </el-row>
             </el-col>
         </el-row>
     `,
