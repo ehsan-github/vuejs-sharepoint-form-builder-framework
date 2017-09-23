39cf977fd6e4dab1ec9c592d28f3d3069b38a98c
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-09-23 14:44:39 +0000

    Adding MasterDetail and remaining Fields

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,14 +2,19 @@
 import { mapState } from 'vuex'
 
 import TextField from './Text'
+import TextAreaField from './TextArea'
 import NumberField from './Number'
 import SelectField from './Select'
 import CustomSelectField from './CustomSelect'
 import DateTimeField from './DateTime'
 import BooleanField from './Boolean'
+import MasterDetail from './MasterDetail'
+import MultiSelectField from './MultiSelect'
+import ChoiceField from './Choice'
+import MultiChoiceField from './MultiChoice'
 
 export default {
-    components: { TextField, NumberField, BooleanField, SelectField, CustomSelectField, DateTimeField },
+    components: { TextField, TextAreaField, NumberField, BooleanField, SelectField, CustomSelectField, DateTimeField, MasterDetail, ChoiceField, MultiChoiceField },
     props: ['fieldId'],
     render () {
         switch (this.fieldType) {
@@ -21,17 +26,27 @@ export default {
             return <BooleanField fieldId={this.fieldId} onChange={this.change}></BooleanField>
         case 'Lookup':
             return <SelectField fieldId={this.fieldId} onChange={this.change}></SelectField>
+        case 'LookupMulti':
+            return <MultiSelectField fieldId={this.fieldId} onChange={this.change}></MultiSelectField>
         case 'DateTime':
             return <DateTimeField fieldId={this.fieldId} onChange={this.change}></DateTimeField>
         case 'RelatedCustomLookupQuery':
-            return <CustomSelectField fieldId={this.fieldId} onChange={this.change}></CustomSelectField>
+            return <CustomSelectField fieldId={this.fieldId} multiple={false} onChange={this.change}></CustomSelectField>
+        case 'MasterDetail':
+            return <MasterDetail fieldId={this.fieldId} onChange={this.change}></MasterDetail>
+        case 'Choice':
+            return <ChoiceField fieldId={this.fieldId} onChange={this.change}></ChoiceField>
+        case 'MultiChoice':
+            return <MultiChoiceField fieldId={this.fieldId} onChange={this.change}></MultiChoiceField>
+        case 'Note':
+            return <TextAreaField fieldId={this.fieldId} onChange={this.change}></TextAreaField>
         default:
             return <div>Unexpected Type: {this.fieldType}</div>
         }
     },
     computed: {
         ...mapState({
-            fieldType(state) { return state.fields[this.fieldId].Type }
+            fieldType (state) { return state.fields[this.fieldId].Type }
         })
     },
     methods: {
