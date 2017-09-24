41f9609ce88c1566b876ea005bc27a850bd240cd
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-09-24 15:44:41 +0000

    Adding jalaali datepicker

--- src/widgets/DateTime.js
+++ src/widgets/DateTime.js
@@ -1,22 +1,67 @@
 // @flow
+import Datepicker from 'vue-jalaali-datepicker/vue-jalaali-datepicker-es6'
+import moment from 'moment-jalaali'
 
 export default {
+    components: { Datepicker },
     template: `
-        <el-date-picker
+        <Datepicker
             v-model="model"
-            type="date"
-            placeholder="Pick a day"
+            :date="startTime"
+            :option="option"
+            :limit="limit"
             @change="change">
-        </el-date-picker>
+        </Datepicker>
     `,
     props: ['value'],
     data () {
         return {
-            model: null
+            model: new Date(),
+            startTime: {
+                time: '' // '1396-05-02' (type=day) && '["1396/05/02", "1396/06/02", "1396/07/02"]' (type="multi-day")
+            },
+            option: {
+                type: 'day',
+                week: ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه'],
+                month: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'],
+                format: 'jYYYY-jMM-jDD',
+                placeholder: 'انتخاب کنید',
+                inputStyle: {
+                    'display': 'inline-block',
+                    'padding': '6px',
+                    'line-height': '22px',
+                    'font-size': '16px',
+                    'border': '2px solid #fff',
+                    'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+                    'border-radius': '2px',
+                    'color': '#5F5F5F'
+                },
+                color: {
+                    header: '#ccc',
+                    headerText: '#f00'
+                },
+                buttons: {
+                    ok: 'انتخاب',
+                    cancel: 'انصراف'
+                },
+                overlayOpacity: 0.5, // 0.5 as default
+                dismissible: true // as true as default
+            },
+            timeoption: {
+                type: 'min',
+                week: ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه'],
+                month: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'],
+                format: 'jYYYY-jMM-jDD HH:mm'
+            },
+            limit: [{
+                type: 'weekday',
+                available: [1, 2, 3, 4, 5]
+            }],
         }
     },
     methods: {
-        change(value) {
+        change(newValue) {
+            let value = moment(newValue, 'jYYYY-jMM-jDD HH:mm').format('YYYY/MM/DD HH:mm')
             this.$emit('input', value)
             this.$emit('change', value)
         }
