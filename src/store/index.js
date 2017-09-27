0342977bab4cc3a185b99232eba982d16904ac4e
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-09-27 15:20:54 +0000

    Fixing mistaken id

--- src/store/index.js
+++ src/store/index.js
@@ -59,7 +59,7 @@ const store = new Vuex.Store({
             state.fields = R.assocPath([id, 'rows'], rows, state.fields)
         },
         MDLoadOptions (state, { id, masterId, rowId, options }) {
-            if (R.prop(id, state.fields[masterId].rows)) {
+            if (R.prop(rowId, state.fields[masterId].rows)) {
                 let rows = state.fields[masterId].rows
                 rows[rowId] = R.assocPath([id, 'options'], options, rows[rowId])
                 state.fields = R.assocPath([masterId, 'rows'], rows, state.fields)
