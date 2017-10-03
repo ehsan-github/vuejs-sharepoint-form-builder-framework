ba686361f5f7bdf904d0726710ac35a2ecff9277
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-10-03 11:29:13 +0000

    remoivng header contents

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,26 +3,6 @@
 export default {
     template: `
         <el-row type='flex' justify='center' class='site-header'>
-            <el-col :span='4' class='banner'>
-                بنر
-            </el-col>
-            <el-col :span='4'>
-            </el-col>
-            <el-col class='menu menu-active' :span='2'>
-                جدید
-            </el-col>
-            <el-col class='menu' :span='2'>
-                ویرایش
-            </el-col>
-            <el-col class='menu' :span='2'>
-                حذف
-            </el-col>
-            <el-col class='menu' :span='2'>
-                نمایش
-            </el-col>
-            <el-col class='menu' :span='2'>
-                لیست
-            </el-col>
         </el-row>
     `
 }
