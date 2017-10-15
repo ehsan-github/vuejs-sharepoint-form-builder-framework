cea70265010c2e138f7dc6276219a585434808d5
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-10-15 14:37:11 +0000

    Refactor

--- src/widgets/CustomSelect.js
+++ src/widgets/CustomSelect.js
@@ -8,7 +8,8 @@ export default {
         <el-select
             v-validate.initial="rules"
             :class="{'error-box': hasError}"
-            :name='name'filterable
+            :name='name'
+            filterable
             v-model="model"
             placeholder="انتخاب"
             @change="change"
