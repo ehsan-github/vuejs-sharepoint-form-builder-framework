89e20475a3312b16d0411df98d25de7f28a255ee
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-10-15 14:33:26 +0000

    Fixing validation problem of Number Field

--- src/widgets/Number.js
+++ src/widgets/Number.js
@@ -3,12 +3,18 @@
 export default {
     inject: ['$validator'],
     template: `
-        <div>
-            <el-input-number :name="name" v-validate="rules" v-model="model" :controls="false" size="small" @change="change"></el-input-number>
-            <div>
-                <span class="error" v-show="$validator.errors.has(name)">{{ $validator.errors.first(name) }}</span>
-            </div>
-        </div>
+    <el-tooltip :disabled="!hasError" class="item" effect="dark" :content="firstError" placement="top-start">
+            <el-input
+                v-validate.initial="rules"
+                :class="{'error-box': hasError}"
+                :name="name"
+                v-model="model"
+                :controls="false"
+                size="small"
+                @change="change"
+            >
+            </el-input>
+    </el-tooltip>
     `,
     props: ['value', 'rules', 'name'],
     data () {
@@ -16,10 +22,14 @@ export default {
             model: null
         }
     },
+    computed: {
+        hasError() { return this.$validator.errors.has(this.name) },
+        firstError() { return this.$validator.errors.first(this.name) }
+    },
     methods: {
         change (value) {
             setTimeout(() => {
-                this.model = value|0
+                this.model = value | 0
                 this.$emit('input', value)
                 this.$emit('change', value)
             }, 50)
