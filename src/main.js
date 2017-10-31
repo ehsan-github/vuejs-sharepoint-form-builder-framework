4b2546b7e169b80fa5df7748df7a3d517d16740b
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-10-31 15:22:27 +0000

    Add class of ```new``` to app

--- src/main.js
+++ src/main.js
@@ -87,7 +87,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    template: '<App/>',
+    template: '<App class="new"/>',
     components: { App },
     store
 })
