5e5433729e6b80ad68d9e4a0d2a0d64e79e68fee
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-11-14 09:04:53 +0000

    Change model of Boolean to value

--- src/widgets/Boolean.js
+++ src/widgets/Boolean.js
@@ -2,22 +2,14 @@
 
 export default {
     template: `
-        <el-checkbox v-model="model" @change="change"></el-checkbox>
+        <el-checkbox :value="value == 1" @change="change"></el-checkbox>
     `,
     props: ['value'],
-    data () {
-        return {
-            model: 0
-        }
-    },
     methods: {
         change(event) {
             let value = event.target.checked ? '1' : '0'
             this.$emit('input', value)
             this.$emit('change', value)
         }
-    },
-    mounted() {
-        this.model = this.value == 1 ? true : false
     }
 }
