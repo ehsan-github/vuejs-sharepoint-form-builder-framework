c2e0a3f90089cc538f7d24d77a47fbaf537f941d
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-11-20 16:25:02 +0000

    Removed Contract Specification from template

--- src/templates/TwoColumn.js
+++ src/templates/TwoColumn.js
@@ -1,16 +1,13 @@
 // @flow
 import Field from '../components/FieldWrapper'
-import ContractSpecForm from '../components/ContractSpec'
-import { mapState, mapGetters } from 'vuex'
-import R from 'ramda'
+import { mapGetters } from 'vuex'
 
 export default {
     template: `
     <div>
-        <ContractSpecForm />
         <el-row justify="start" :gutter="24">
                 <el-col v-for='(f, id) in fields' :key='id' :span="f.Type == 'MasterDetail' ? 24 : 12">
-            <el-form ref='form' :model='form' label-position="top">
+            <el-form ref='form' label-position="top">
                     <el-form-item :key='id' :class="{require: f.IsRequire}" :label='f.Type == "MasterDetail" ? null : f.Title' :prop='id'>
                         <Field :fieldId='id' ref='fields' @change='v => change(id, v)' />
                     </el-form-item>
@@ -19,23 +16,10 @@ export default {
         </el-row>
     </div>
     `,
-    components: { Field, ContractSpecForm },
-    data () {
-        return {
-            form: {}
-        }
-    },
+    components: { Field },
     computed: {
-        ...mapState({
-            specs: s => s.contractSpecs,
-        }),
         ...mapGetters({
             fields: 'filteredFields'
         })
-    },
-    methods: {
-        change (id, value) {
-            this.form = R.assoc(id, value, this.form)
-        }
     }
 }
