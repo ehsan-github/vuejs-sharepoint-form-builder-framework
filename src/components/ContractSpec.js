4ff180ceebd6e168d4ea6962556dd99f1fe586b1
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-11-28 09:59:11 +0000

    Fixing CSS of Contract Spec.

--- src/components/ContractSpec.js
+++ src/components/ContractSpec.js
@@ -5,31 +5,34 @@ export default {
     template: `
     <div class="contractForm">
         <h3>مشخصات پیمان</h3>
-        <el-form ref='form' label-position="top">
         <el-row justify="center" :gutter="24">
             <el-col :span="12">
-                <el-form-item class="contractSpecs" label="پیمان">{{Title}}</el-form-item>
+                <div class="field-title"> پیمان </div>
+                <div class="master-item">{{Title}}</div>
             </el-col>
             <el-col :span="12">
-                <el-form-item class="contractSpecs" label="حوزه">{{Area}}</el-form-item>
+                <div class="field-title"> حوزه </div>
+                <div class="master-item">{{Area}}</div>
+                </el-form>
             </el-col>
             <el-col :span="12">
-                <el-form-item class="contractSpecs" label="پیمانکار">{{Contractor}}</el-form-item>
+                <div class="field-title"> پیمانکار </div>
+                <div class="master-item">{{Contractor}}</div>
             </el-col>
             <el-col :span="12">
-                <el-form-item class="contractSpecs" label="مشاور">{{Consultant}}</el-form-item>
+                <div class="field-title"> مشاور  </div>
+                <div class="master-item">{{Consultant}}</div>
             </el-col>
         </el-row>
-        </el-form>
     </div>
     `,
     computed: {
         ...mapState({
             specs: s => s.contractSpecs,
         }),
-        Title () { return this.specs.Title || 'در حال بارگذاری' },
-        Area () { return this.specs.Area ? this.specs.Area.Title : 'در حال بارگذاری' },
-        Contractor () { return this.specs.Contractor ? this.specs.Contractor.Title : 'در حال بارگذاری' },
-        Consultant () { return this.specs.Consultant ? this.specs.Consultant.Title : 'در حال بارگذاری' },
+        Title () { return this.specs.Title || '' },
+        Area () { return this.specs.Area ? this.specs.Area.Title : '' },
+        Contractor () { return this.specs.Contractor ? this.specs.Contractor.Title : '' },
+        Consultant () { return this.specs.Consultant ? this.specs.Consultant.Title : '' },
     }
 }
