96241ac6b0afcb8295e4c9c88a2bf29d356d8ed7
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-12-20 08:01:25 +0000

    Description tooltip

--- src/components/MasterDetail/Header.js
+++ src/components/MasterDetail/Header.js
@@ -4,11 +4,17 @@ export default {
     props: ['fields'],
     template: `
     <thead>
-        <span>
+        <span class="header-span">
             <tr>
                 <th class="button"></th>
                 <th class="radif">ردیف</th>
-                <th class='is-leaf' v-for='f in fields' :key='f.Guid' :class="f.Type">{{f.Title}}</th>
+                <th class='is-leaf' v-for='f in fields' :key='f.Guid' :class="f.Type">
+                    <el-tooltip :disabled="f.Description == ''" effect="light" :content="f.Description" placement="top">
+                    <div>
+                    {{f.Title}}
+                    </div>
+                    </el-tooltip>
+                </th>
             </tr>
         </span>
     </thead>
