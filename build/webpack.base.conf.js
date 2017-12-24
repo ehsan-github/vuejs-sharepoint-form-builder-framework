2bb13a5db0a64a50e0310533ae27ac59db26025e
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-12-24 10:11:06 +0000

    AssetsPublicPath config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -55,6 +55,13 @@ module.exports = {
         }
       },
       {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+        }
+      },
+      {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
