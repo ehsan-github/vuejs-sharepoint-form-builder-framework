b0b63857fc03908661866db569cd248baba35f51
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2018-01-01 08:11:16 +0330

    Fixed textArea bug

--- src/widgets/TextArea.js
+++ src/widgets/TextArea.js
@@ -34,7 +34,7 @@ export default {
         }
     },
     mounted() {
-        this.model = this.value.replace(/\\n/g, '\\\n').replace(/\\/g, '')
+        this.model = this.value ? this.value.replace(/\\n/g, '\\\n').replace(/\\/g, '') : this.value
     }
 }
 
